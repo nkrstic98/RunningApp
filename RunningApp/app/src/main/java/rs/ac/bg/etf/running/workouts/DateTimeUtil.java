9def40b249bb85c3f8bdcf97d6518a96c097b3dd
nkrstic98 <nikolakrle998@gmail.com>
2020-12-17 22:59:28 +0100

    Add DatePickerFragment

--- RunningApp/app/src/main/java/rs/ac/bg/etf/running/workouts/DateTimeUtil.java
+++ RunningApp/app/src/main/java/rs/ac/bg/etf/running/workouts/DateTimeUtil.java
@@ -1,10 +1,16 @@
 package rs.ac.bg.etf.running.workouts;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class DateTimeUtil {
+
     private static final SimpleDateFormat simpleDateFormat =
             new SimpleDateFormat("dd/MM/yyyy");
+
+    private static final Calendar calendar =
+            Calendar.getInstance();
 
     public static SimpleDateFormat getSimpleDateFormat() {
         return simpleDateFormat;
@@ -13,7 +19,11 @@ public class DateTimeUtil {
     public static String realMinutesToString(double realMinutes) {
         int minutes = (int) realMinutes;
         int seconds = (int) (60 * (realMinutes - minutes));
-
         return minutes + ":" + String.format("%02d", seconds);
+    }
+
+    public static Date getDate(int year, int month, int day) {
+        calendar.set(year, month, day);
+        return calendar.getTime();
     }
 }
--- RunningApp/app/src/main/java/rs/ac/bg/etf/running/workouts/WorkoutCreateFragment.java
+++ RunningApp/app/src/main/java/rs/ac/bg/etf/running/workouts/WorkoutCreateFragment.java
@@ -1,5 +1,6 @@
 package rs.ac.bg.etf.running.workouts;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -13,16 +14,23 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.Date;
+
 import rs.ac.bg.etf.running.MainActivity;
-import rs.ac.bg.etf.running.R;
 import rs.ac.bg.etf.running.databinding.FragmentWorkoutCreateBinding;
 
 public class WorkoutCreateFragment extends Fragment {
+
+    public static final String REQUEST_KEY = "date-picker-request";
 
     private FragmentWorkoutCreateBinding binding;
     private WorkoutViewModel workoutViewModel;
     private NavController navController;
     private MainActivity mainActivity;
+
+    public WorkoutCreateFragment() {
+        // Required empty public constructor
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -33,13 +41,25 @@ public class WorkoutCreateFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(
+            @NonNull LayoutInflater inflater,
+            ViewGroup container,
+            Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
         binding = FragmentWorkoutCreateBinding.inflate(inflater, container, false);
 
         binding.toolbar.setNavigationOnClickListener(
-                view -> navController.navigateUp()
-        );
+                view -> navController.navigateUp());
+
+        binding.workoutDateEditText.setOnClickListener(
+                view -> new DatePickerFragment().show(getChildFragmentManager(), null));
+
+        getChildFragmentManager().setFragmentResultListener(REQUEST_KEY, this,
+                (requestKey, result) -> {
+                    Date date = (Date) result.getSerializable(DatePickerFragment.SET_DATE_KEY);
+                    String dateForEditText = DateTimeUtil.getSimpleDateFormat().format(date);
+                    binding.workoutDate.getEditText().setText(dateForEditText);
+                });
 
         return binding.getRoot();
     }
@@ -49,4 +69,5 @@ public class WorkoutCreateFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         navController = Navigation.findNavController(view);
     }
+
 }
