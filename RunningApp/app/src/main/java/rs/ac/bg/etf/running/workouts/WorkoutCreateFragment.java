4f6dc0da00d836e6ef9df172ac596a582021f35c
nkrstic98 <nikolakrle998@gmail.com>
2020-12-17 21:23:21 +0100

    Add WorkoutCreateFragment

---
+++ RunningApp/app/src/main/java/rs/ac/bg/etf/running/workouts/WorkoutCreateFragment.java
@@ -0,0 +1,52 @@
+package rs.ac.bg.etf.running.workouts;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import rs.ac.bg.etf.running.MainActivity;
+import rs.ac.bg.etf.running.R;
+import rs.ac.bg.etf.running.databinding.FragmentWorkoutCreateBinding;
+
+public class WorkoutCreateFragment extends Fragment {
+
+    private FragmentWorkoutCreateBinding binding;
+    private WorkoutViewModel workoutViewModel;
+    private NavController navController;
+    private MainActivity mainActivity;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mainActivity = (MainActivity) requireActivity();
+        workoutViewModel = new ViewModelProvider(mainActivity).get(WorkoutViewModel.class);
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentWorkoutCreateBinding.inflate(inflater, container, false);
+
+        binding.toolbar.setNavigationOnClickListener(
+                view -> navController.navigateUp()
+        );
+
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        navController = Navigation.findNavController(view);
+    }
+}
