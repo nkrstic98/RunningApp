b4760fc3c2d6af5f51d1947903e46abe6729849e
nkrstic98 <nikolakrle998@gmail.com>
2021-02-02 13:26:14 +0100

    Move motivation message into service

--- RunningApp/app/src/main/java/rs/ac/bg/etf/running/workouts/WorkoutService.java
+++ RunningApp/app/src/main/java/rs/ac/bg/etf/running/workouts/WorkoutService.java
@@ -2,12 +2,39 @@ package rs.ac.bg.etf.running.workouts;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Looper;
+import android.os.SystemClock;
 import android.util.Log;
+import android.widget.Toast;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 import rs.ac.bg.etf.running.MainActivity;
+import rs.ac.bg.etf.running.R;
 
 public class WorkoutService extends Service {
+
+    private Timer timer = new Timer();
+
+    private void scheduleTimer() {
+
+        Handler handler = new Handler(Looper.getMainLooper());
+
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(() -> Toast.makeText(
+                        WorkoutService.this,
+                        getResources().getStringArray(
+                                R.array.workout_toast_motivation)[0],
+                        Toast.LENGTH_SHORT).show()
+                );
+            }
+        }, 0, 7000);
+    }
 
     @Override
     public void onCreate() {
@@ -18,6 +45,7 @@ public class WorkoutService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.d(MainActivity.LOG_TAG, "WorkoutService.onStartCommand()");
+        scheduleTimer();
         return super.onStartCommand(intent, flags, startId);
     }
 
@@ -29,8 +57,8 @@ public class WorkoutService extends Service {
 
     @Override
     public void onDestroy() {
-
         Log.d(MainActivity.LOG_TAG, "WorkoutService.onDestroy()");
         super.onDestroy();
+        timer.cancel();
     }
 }
