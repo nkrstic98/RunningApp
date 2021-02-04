a74ecc977c2877b8b07667a7cbca08a50805cdeb
nkrstic98 <nikolakrle998@gmail.com>
2021-02-04 14:56:54 +0100

    Add LifecycleAwareLocator without permissions

--- RunningApp/app/src/main/java/rs/ac/bg/etf/running/workouts/WorkoutService.java
+++ RunningApp/app/src/main/java/rs/ac/bg/etf/running/workouts/WorkoutService.java
@@ -50,6 +50,9 @@ public class WorkoutService extends LifecycleService {
     @Inject
     public LifecycleAwareMeasurer measurer;
 
+    @Inject
+    public LifecycleAwareLocator locator;
+
     @Override
     public void onCreate() {
         Log.d(MainActivity.LOG_TAG, "WorkoutService.onCreate()");
@@ -58,6 +61,7 @@ public class WorkoutService extends LifecycleService {
         getLifecycle().addObserver(motivator);
         getLifecycle().addObserver(player);
         getLifecycle().addObserver(measurer);
+        getLifecycle().addObserver(locator);
     }
 
     @Override
@@ -76,6 +80,7 @@ public class WorkoutService extends LifecycleService {
                     motivator.start(this);
                     player.start(this);
                     measurer.start(this);
+                    locator.getLocation(this);
                 }
                 break;
             case INTENT_ACTION_POWER:
